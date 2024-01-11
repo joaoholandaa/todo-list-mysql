2949bd336a02b1b400f005111aad8eae26803f55
Joao Holanda <joaocmdt505@gmail.com>
2024-01-11 13:27:13 -0300

    função para obter as tarefas do banco de dados

--- view.py
+++ view.py
@@ -12,9 +12,15 @@ meubd = mysql.connector.connect(
 # Criando cursor
 cursor = meubd.cursor()
 
-#Função para adcionar tarefa ao bando de dados
+# Função para adcionar tarefa ao banco de dados
 def adcionar_tarefas(i):
   sql = "INSERT INTO tarefas (descricao, concluido) VALUES (%s, %s)"
   valores = (i, False)
   cursor.execute(sql, valores)
   meubd.commit()
+
+# Função para obter todas as tarefas do banco de dados
+def obter_tarefas():
+  sql = "SELECT * FROM tarefas"
+  cursor.execute(sql)
+  return cursor.fetchall()
